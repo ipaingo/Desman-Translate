c37a074f67b083716a9ec6919d50df36d454ead7
ipaingo <szimenkova7@gmail.com>
2023-12-26 19:33:35 +0300

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,201 @@
+import { Link } from "react-router-dom"
+import Navbar from "./Navbar"
+import Footer from "./Footer"
+import placeholder from "../images/placeholder.png"
+import React, { useEffect, useState } from "react"
+import Button from "react-bootstrap/Button"
+import { useNavigate } from "react-router-dom"
+
+import api_link from "../App"
+
+var user = ""
+if (localStorage.getItem("user") != null) {
+  user = JSON.parse(localStorage.getItem("user"))
+}
+console.log(user)
+
+function Home() {
+  const [projects, setProjects] = useState([]);
+
+  const [recentProjects, setRecentProjects] = useState([]);
+
+  function Fill_projects() {
+    fetch("/api/projects")
+      .then((response) => response.json())
+      .then((responseData) => {
+        if (user != "") {
+
+          var answer = []
+          var user_projects = new Array()
+          responseData.forEach(elem => {
+            if (elem.owner_id === user.id)
+              user_projects.push(elem)
+          })
+
+          user_projects.sort(function (a, b) {
+            return a.created_at < b.created_at;
+          });
+
+          let count = 0
+
+          user_projects.forEach(elem => {
+            if (count != 2) {
+              console.log("dfghjkljhgf")
+              console.log(elem)
+
+
+              let item =
+                <div className="container text-left" style={{ paddingBottom: 10 }}>
+                  <div
+                    className="row border rounded py-3 align-items-center"
+                    style={{ marginTop: 5 }}
+                  >
+                    <div className="col-2">
+                      <img
+                        width={60}
+                        height={60}
+                        src={placeholder}
+                        alt="thumbnail"
+                        style={{ marginRight: 10 }}
+                      />
+                    </div>
+                    <div className="col text-left">
+
+                      <Link to={"/projects/" + elem.handle} className="link-primary">
+                        {elem.name}
+                      </Link>
+                      <br /> Полезная информация
+                    </div>
+                  </div>
+                </div>
+
+              answer.push(item)
+              // console.log(item)
+            }
+          })
+          setProjects(answer)
+        }
+
+      })
+      .catch(error => console.warn(error));
+  }
+
+
+  async function Fill_recent_projects() {
+    await fetch("/api/projects")
+      .then((response) => response.json())
+      .then((responseData) => {
+
+        responseData.sort(function (a, b) {
+          return a.created_at < b.created_at;
+        });
+        let count = 0
+        var answer = []
+        responseData.forEach(elem => {
+          if (count != 2) {
+            count++
+            console.log("dfghjkljhgf")
+            console.log(elem)
+
+
+            let item =
+              <div className="container text-left" style={{ paddingBottom: 10 }}>
+                <div
+                  className="row border rounded py-3 align-items-center"
+                  style={{ marginTop: 5 }}
+                >
+                  <div className="col-2">
+                    <img
+                      width={60}
+                      height={60}
+                      src={placeholder}
+                      alt="thumbnail"
+                      style={{ marginRight: 10 }}
+                    />
+                  </div>
+                  <div className="col text-left">
+
+                    <Link to={"/projects/" + elem.handle} className="link-primary">
+                      {elem.name}
+                    </Link>
+                    <br /> Полезная информация
+                  </div>
+                </div>
+              </div>
+
+            answer.push(item)
+            // console.log(item)
+          }
+          else
+            setRecentProjects(answer)
+
+        })
+      })
+      .catch(error => console.warn(error));
+  }
+
+
+  useEffect(() => {
+    Fill_projects();
+  }, []);
+
+  useEffect(() => {
+    Fill_recent_projects();
+  }, []);
+
+
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = '/create';
+    navigate(path);
+  }
+
+  return (
+    <>
+      <Navbar />
+      <div className="container" style={{ marginTop: 50 }}>
+        <div className="row">
+          <div className="col-6 text-left" style={{ paddingRight: "5%" }}>
+            <h2>Недавние проекты:</h2>
+            {projects}
+
+            <h2 style={{ marginTop: 20 }}>Популярные проекты:</h2>
+            {recentProjects}
+
+
+          </div>
+          <div
+            className="col border-top border-start rounded py-3"
+            style={{ marginTop: 5, paddingLeft: 20 }}
+          >
+            <h5 className="py-2 border-bottom" style={{ marginTop: "-10px" }}>
+              Что такое Desman Translate?
+            </h5>
+            <p>
+              Добро пожаловать в веб-сервис для коллективных переводов! Desman Translate предназначен для совместных переводов книг, программ, субтитров и всего на свете, что имеет форму текста.
+            </p>
+            <h5 className="py-2 border-bottom" style={{ marginTop: "-10px" }}>
+              Как это работает?
+            </h5>
+            <p>
+              Когда вы загружаете текст для работы, он разбивается на небольшие отрывки: строки, абзацы, отдельные субтитры — вы можете выбрать способ самостоятельно. Каждый пользователь может предложить свой вариант перевода для отрывка, а лучший вариант определяется голосованием.
+            </p>
+            <p>
+              Присоединяйтесь к командам переводчиков, создавайте собственные проекты, приглашайте других пользователей присоединиться, переводите книги, программы и субтитры, оттачивайте свои навыки и создавайте лучший перевод любого текста.
+            </p>
+            <p>Have a lot of fun...</p>
+            <Button variant="primary"
+              style={{ marginTop: "-10px" }}
+              onClick={routeChange}
+            >
+              Создать проект
+            </Button>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
---
+++ src/pages/Login.js
@@ -0,0 +1,112 @@
+import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Button from "react-bootstrap/Button";
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom";
+
+export default function Login() {
+    const navigate = useNavigate();
+
+    const [inputMail, setInputMail] = useState("");
+    const [inputPass, setInputPass] = useState("");
+
+    const mailChange = event => setInputMail(event.target.value);
+
+    const passChange = event => setInputPass(event.target.value);
+
+    const [errorVisibility, setErrorVisibility] = useState(false);
+
+    function toggle() {
+        setErrorVisibility((errorVisibility) => "true");
+    }
+
+    async function Submit(event) {
+        event.preventDefault()
+        console.log(JSON.stringify({
+            "username": inputMail,
+            "password": inputPass
+        }))
+        let jopa = await fetch("/api/login",
+            {
+                method: "POST",
+                body: JSON.stringify({
+                    "username": inputMail,
+                    "password": inputPass
+                }),
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8',
+                },
+                credentials: "include",
+
+            }).then(response => {
+                if (!response.ok) {
+                    toggle()
+                }
+                else return response.json()
+            }).then(data => {
+                if (typeof (data) != "undefined") {
+                    console.log(typeof (data))
+                    localStorage.setItem("user", JSON.stringify(data))
+                    console.log(data)
+                    navigate("../")
+                    window.location.reload(false);
+                }
+            })
+    }
+
+
+
+
+    return (
+        <>
+            <Navbar />
+            <div
+                className="container text-left"
+                style={{
+                    marginTop: 50,
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    width: "20%",
+                    minWidth: 300
+                }}
+            >
+                <h1 style={{ marginBottom: 20 }}>Вход</h1>
+                <form>
+                    <div className="mb-3">
+                        <label htmlFor="inputEmailLogin" className="form-label">
+                            Имя пользователя
+                        </label>
+                        <input className="form-control" id="inputEmailLogin" onChange={mailChange} />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="inputPasswordLogin" className="form-label">
+                            Пароль
+                        </label>
+                        <input
+                            onChange={passChange}
+                            type="password"
+                            className="form-control"
+                            id="inputPasswordLogin"
+                            aria-describedby="forgotPassword"
+                        />
+                        <div id="forgotPassword" className="form-text">
+                            Забыли пароль?
+                        </div>
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="rememberCheck" />
+                        <label className="form-check-label" htmlFor="rememberCheck">
+                            Запомнить аккаунт
+                        </label>
+                    </div>
+                    {errorVisibility && <div id="error" style={{ margin: "5px 0px" }}>Неверный логин или пароль.</div>}
+                    <Button type="submit" variant="primary" onClick={Submit}>
+                        Войти
+                    </Button>
+                </form>
+            </div>
+            <Footer />
+        </>
+    );
+}
